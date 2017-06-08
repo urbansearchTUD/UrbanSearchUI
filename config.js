252e53ee7aa7baa72038ec7a62adb2555578d759
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-06-08 11:41:31 +0200

    Move private config to different file

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const config = require('./config.json')
+const privateConfig = require('./config_private.json')
 
 function get(key) {
-    if (key in config.custom) {
-        return config.custom[key]
-    } else if (key in config.default) {
-        return config.default[key]
+    if (key in privateConfig) {
+        return privateConfig[key]
+    } else if (key in config) {
+        return config[key]
     } else {
         return null
     }
 }
 
 function set(key, value) {
-    config.custom[key] = value
+    privateConfig[key] = value
 }
 
 module.exports = {
