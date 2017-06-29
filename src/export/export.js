0af4ad23b9e6c422650473aa0afef7e63ee4f30a
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-06-29 12:00:46 +0200

    Add fugly export button

---
+++ src/export/export.js
@@ -0,0 +1,7 @@
+config = require('../../config')
+
+API_URL = config.get('api_url')
+API_URL = 'http://localhost:5000/api/v1/'
+
+BTN = document.querySelector('#exportBtn')
+BTN.href = API_URL + 'data/export_all'
